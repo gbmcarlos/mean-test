7f1f7d00f1a7407d91ab31c6ba41c884e4b65b3a
gbmcarlos <gbmcarlos@gmail.com>
2016-03-02 16:33:48 +0100

    Front views functional, missing real request

--- public/javascripts/Comparator/ParametersController.js
+++ public/javascripts/Comparator/ParametersController.js
@@ -2,47 +2,141 @@
  * Created by gbmcarlos on 3/2/16.
  */
 
-ComparatorModule.controller('ParametersController', ['$scope', function($scope) {
+ComparatorModule.controller('ParametersController', ['$scope', '$http', function($scope, $http) {
+
+    $scope.requestData = {
+        filter: {
+            quickFilter: [],
+            providers: []
+        },
+        email_address: $scope.emailAddress,
+        features: '',
+        userSalary: '',
+        apiCountry: 'ae',
+        apiLanguage: 'en',
+        categoryId: '543857a388de100000ae90bb',
+        limit: 25,
+        pageNum: 0,
+        order: false
+    };
+
+    $scope.addQuickFilter = function(value, state) {
+
+        if (state) {
+            requestData.filter.quickFilter.push(value);
+        } else {
+            requestData.filter.quickFilter.splice(requestData.filter.quickFilter.indexOf(value), 1);
+        }
+
+    };
+
+    $scope.addProvider = function(value, state) {
+
+
+        if (state) {
+            requestData.filter.providers.push(value);
+        } else {
+            requestData.filter.providers.splice(requestData.filter.providers.indexOf(value), 1);
+        }
+
+    };
+
+    $scope.getResults = function() {
+
+        $http({
+            method: 'GET',
+            url: '/api-wrapper',
+            params: $scope.requestData
+        }).then(function successCallback(response) {
+            console.log(response);
+        });
+
+    };
 
     $scope.quickFilters = [
         {
             label: 'Shari’ah Compliant Only',
-            value: 'islamic_finance'
+            value: 'islamic_finance',
+            state: false
         },
         {
             label: 'No Annual Fee',
-            value: 'monthly_fee'
+            value: 'monthly_fee',
+            state: false
         },
         {
             label: 'Offered For Non-Listed Companies',
-            value: 'cnl_product'
+            value: 'cnl_product',
+            state: false
         }
     ];
 
     $scope.providers = [
         {
             label: 'ADCB',
-            id: '543857e688de100000b05978'
+            id: '543857e688de100000b05978',
+            state: false
         },
         {
             label: 'ADIB',
-            id: '543857e688de100000b059dd'
+            id: '543857e688de100000b059dd',
+            state: false
         },
         {
             label: 'Ajman Bank',
-            id: '543857e788de100000b05afb'
+            id: '543857e788de100000b05afb',
+            state: false
         },
         {
             label: 'Al Hilal Bank',
-            id: '543857e788de100000b05b0b'
+            id: '543857e788de100000b05b0b',
+            state: false
         },
         {
             label: 'Al Khaliji France',
-            id: '543857e688de100000b059f9'
+            id: '543857e688de100000b059f9',
+            state: false
         },
         {
             label: 'AMEX',
-            id: '566e6e76b2c18f11375a1cc0'
+            id: '566e6e76b2c18f11375a1cc0',
+            state: false
+        }
+    ];
+
+    $scope.creditCardsTypes = [
+        {
+            label: 'Select a feature',
+            value: '',
+            selected: true
+        },
+        {
+            label: 'Cashback',
+            value: 'cashback'
+        },
+        {
+            label: 'Balance Transfer',
+            value: 'balance-transfer'
+        },
+        {
+            label: 'Air Miles',
+            value: 'airlines'
+        },
+        {
+            label: 'Dining Privileges',
+            value: 'dining'
+        },
+        {
+            label: 'Golf Privileges',
+            value: 'golf'
+        },
+        {
+            label: 'Valet Parking',
+            value: 'valet'
+        },
+        {
+            label: 'Cinema Tickets',
+            value: 'cinema'
         }
     ];
 
